e381aa17779a378986a9ba20ab468ec130a29547
Feyza Çakmak <83360319+feyzacakmakk@users.noreply.github.com>
2021-05-04 06:35:52 +0300

    Add files via upload

---
+++ homework3/src/homework3/User.java
@@ -0,0 +1,40 @@
+package homework3;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	private String userName;
+	
+	public User() {
+		
+	}
+	
+	public User(String firstName, String lastName, String userName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+
+}
